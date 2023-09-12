755879951238bc23011e68ee674fec222d25af51
Ayan776 <143824843+Ayan776@users.noreply.github.com>
2023-09-12 04:54:15 -0700

    Add files via upload

---
+++ app 2 .js
@@ -0,0 +1,242 @@
+// Chapter 6
+// task 1
+// ++1
+
+// task 2
+
+// --100
+
+// task 3
+
+// var x = 50;
+// var y = x++;
+// 51
+
+// task 4
+// var y = 50;
+// var z = --y;
+// 49
+
+// task 5
+// var newNum = --num;
+
+// task 6
+// var originalValue = 7;
+// var newValue = originalValue + 1;
+
+// task 7
+// var myNumber = 56
+// myNumber++
+// alert("my answer is" + " " + myNumber)
+
+// Chapter 7
+
+// task 1
+// var calculatedNum = 2 + (2 * 6);
+// alert(calculatedNum)
+// answer 14
+
+// task 2
+// var calculatedNum = (2 + 2) * 6;
+// alert(calculatedNum)
+// answer is 24
+
+// task 3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// alert(calculatedNum)
+// answer is 24
+
+// task 4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// alert(calculatedNum)
+// answer is 18
+
+// task 5
+// var cost = (2 + 2) * 4 + 10;
+
+// task 6
+// var units = 2 + (2 * 4) + 10;
+
+// task 7
+// var pressure = (4 / 2) * 4;
+
+// Chapter 8
+
+// task 1
+// var num = "2" + "2";
+// alert(num)
+// answer 22
+
+// task 2
+// message = ("Hello," + "Dolly");
+// alert("Hello," + "Dolly")
+
+// task 3
+// alert("33" + 3);
+// answer 333
+
+// task 4
+// message = ("Pakistan" + "Zindabad");
+// alert("Pakistan" + " " + "Zindabad");
+
+// task 5
+// var task = "ayanfaisal" + 457;
+// alert(task)
+
+// task 6
+// var string1 = "hello"
+// var string2 = "world"
+// var res = string1 + string2;
+// alert(string1 + string2)
+
+// Chapter 9
+
+// task 1
+// var firstName = prompt("Enter first name");
+
+// task 2
+// var country = prompt("Country?", "China");
+
+// task 3
+// var yourName = prompt("Enter Your Name");
+
+// task 4
+// var string = prompt("Whats your name" , "Input")
+
+// task 5
+// var string1 = "Whats your name"
+// var string2 = "Ayan"
+// var res = prompt(string1 , string2 )
+
+// task 6
+// var message = "What is your favorite color?";
+// var defaultResponse = "Blue";
+
+// var userResponse = prompt(message, defaultResponse);
+
+// if (userResponse !== null) {
+//   alert("Your favorite color is: " + userResponse);
+// } else {
+//   alert("You didn't enter a response.");
+// }
+
+// Chapter 10
+
+// task 1
+// var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// }
+// correct
+
+// task 2
+// if (x === y) {
+//     var z = prompt("Enter the value of z?");
+//   }
+
+// task 3
+// var ZipCode = "10010";
+
+// if (Zipode === "10010"){
+//     alert("karachi")
+// }
+// else{
+//     alert("Please write correct city")
+// }
+
+// task 4
+// var x = 7; 
+
+// if (x === 7) {
+//   x = 7; 
+// }
+
+// Chapter 11
+
+// task 1
+// if (variable1 != variable2) {
+//   }
+
+// task 2
+// if (variable1 >= variable2) {
+//   }
+
+// task 3
+// var myVariable = 5; 
+
+// if (myVariable !== 10) {
+//   myVariable = 20; 
+// }
+
+// task 4
+// var number1 = 10; 
+// var number2 = 15; 
+
+// if (number1 !== number2) { 
+//   alert("Congratulations!"); }
+
+// task 5
+// var enteredName = prompt("Please enter your first name:");
+// var anotherName = "Ayan"; 
+// if (enteredName !== anotherName) {
+//   alert("No match");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
