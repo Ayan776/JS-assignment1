0e0292915c97c261518ae2503ef45d2bbbd36a53
Ayan776 <143824843+Ayan776@users.noreply.github.com>
2023-09-15 05:18:03 -0700

    Add files via upload

---
+++ app 3.js
@@ -0,0 +1,16 @@
+// task 3
+// var arr = ["accounts", "business" , "economics" ,"pst" , "maths"]
+// console.log(arr)
+
+// task 4
+// var numbers = [1, 2, 3, 4, 5];
+
+// task 5
+// var booleanArray = [true, false, true, false];
+
+// task 6
+// var arr = [1, "Hello", true, null, 7]; 
+
+// task 7
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write(qualifications)
