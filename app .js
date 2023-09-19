145941521f26d6f07cd52382d15c1e67a6d7df6d
Ayan776 <143824843+Ayan776@users.noreply.github.com>
2023-09-19 09:26:58 -0700

    Add files via upload

---
+++ app .js
@@ -0,0 +1,67 @@
+// least and highest number loop array
+
+// var arr = [8,4,87,3,5,7,2,56,10,11]
+// var min = arr[0]
+// for(i = 0 ; i <= arr.length; i++){
+//     if(arr[i] > min){
+//         min = arr[i]
+//     }
+// }
+// alert(min)
+
+// city 
+
+// var userInput =prompt("where do u live ")
+// var flags =false
+// var city = ["multan","gujranwala","punjab","Rawalpindi"]
+// for( i = 0; i < city.length; i++){
+//     if(
+//         userInput === city[i]
+//     ){
+//       flags = true;
+//     }
+// }
+// if(flags){
+//     alert("noice")
+// }
+// else{alert("not noice")}
+
+// Subject Selection                                                                                                                                                  
+           
+// var userInput = prompt("what subject u chose")
+// userInput = userInput.toLowerCase( );
+// var flags = false
+// var subjects = ["Computer","Commerce","Bio"]
+// for(i = 0; i < subjects.length ; i++){
+//     if (userInput === subjects[i]){
+//      flags = true
+//     }
+// }
+// if(flags){
+//     alert("come to try out our coaching center")
+// }
+// else{alert("sorry no matches")}
+
+// Task
+
+// var userInput = prompt("write a palindrome name")
+// var check = []
+// for ( var i =  userInput.length ;i >=0 ; i--){
+// console.log(userInput[i])
+// }
+// if(userInput |= check ){
+//     console.log("match")
+// }
+// else{console.log("not match")}
+// WRONG
+
+// var userInput = prompt("Enter a word")
+// var check = '';
+// for(var i = userInput.length -1; i >= 0 ; i--){
+//     check += userInput[i]
+// }
+// if(userInput === check){
+//     console.log("its working")
+// }
+// else{   console.log("its not working")}
+// CORRECT :>
