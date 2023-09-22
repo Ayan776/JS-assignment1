5309c5578a9b70acb4b4a4e84331d8ed1b9a8d0f
Ayan776 <143824843+Ayan776@users.noreply.github.com>
2023-09-22 09:15:45 -0700

    Update app .js

--- app .js
+++ app .js
@@ -65,3 +65,64 @@
 // }
 // else{   console.log("its not working")}
 // CORRECT :>
+
+// CLass 6
+// var userInput = prompt("enter anything which u like to eat")
+// for(i = 0; i <= userInput.length; i++){
+//     if(userInput.slice(i,i+2))
+//     alert("double gap Aquired")
+// }
+
+// var userInput = prompt("where are you from")
+// var firstChar = userInput.slice(0,1).toUpperCase
+// var remainChar = userInput.slice(1).toLowerCase
+// var countryArr = ["Pakistan","India","bangladesh","Iran","Iraq"]
+// for(i = 0; i<= countryArr.length; i++){
+// if(userInput === countryArr[i]){
+//     console.log("match")                                                                   
+// }
+// }
+
+// DEFAULT METHOD TO REPLACE
+
+// var str = "The New Yorker magzine doesnt allow the phrase World War II They say it should be the Second World War. so Lets search the following sentence for the banned chracters and replace them with the phrase that the New Yorker prefers"
+// for( i = 0; i<= str.length ; i++){
+//     if ( str.slice( i,i+12) === "World War II"){
+//         str=str.slice(0 , i)+"Second World War" + str.slice(i+12)
+    
+//     }
+
+// }
+
+// console.log(str)
+
+// INDEX OF METHOD TO REPLACE
+// var str = "The New Yorker magzine doesnt allow the phrase World War II They say it should be the Second World War. so Lets search the following sentence for the banned chracters and replace them with the phrase that the New Yorker prefers"
+
+// var indexNumber = str.indexOf("banned chracters")
+
+// var firstText = str.slice(0,indexNumber)
+// var replace = "unbanned chracters"
+// var remainingText = str.slice(indexNumber + 16)
+// console.log(firstText + replace + remainingText)
+
+// REPLACE METHOD TO REPLACE
+// var str = "The New Yorker magzine doesnt allow the phrase World War II They say it should be the Second World War. so Lets search the following sentence for the banned chracters and replace them with the phrase that the New Yorker prefers"
+
+// console.log(str.replace("banned chracters" , "unbanned chracters"))
+
+
+// var number = 4.46
+// console.log(Math.round(number)) NUMBERS ROUNDED
+// console.log(Math.ceil(number))  NUMBERS CEIL
+// console.log(Math.floor(number)) NUMBER FLOOR
+
+// RANDOM NUMBERS
+// var num = Math.random() * 6
+// console.log(num.toFixed(0))
+
+// CONVERTING A STRING INTO NUMBER
+// var num = Number(prompt(" ENTER VALUES TO ADD"))
+// var num2 = Number(prompt(" ENTER VALUES TO ADD"))
+// var res = num + num2
+// console.log(res)
