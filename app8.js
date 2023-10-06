0279ad0f0c1f0b1ac5012547cd306e42822d3511
Ayan776 <143824843+Ayan776@users.noreply.github.com>
2023-10-05 20:23:50 -0700

    Add files via upload

---
+++ app8.js
@@ -0,0 +1,46 @@
+// Task 1
+// function date(){
+//     var rightNow = new Date();
+//     alert(rightNow)
+// }
+// date()
+
+// Task 2
+// function Name(){
+// return firstName + lastName
+//  }
+// var firstName =  prompt("ENTER YOUR FIRST NAME")
+// var lastName = prompt("ENTER YOUR LAST NAME")
+// var res = Name(firstName,lastName)
+// alert(res)
+
+// Task 3
+// function sum(){
+// return a + b
+// }
+// var a =  Number(prompt("ENTER YOUR FIRST Num"))
+// var b = Number(prompt("ENTER YOUR LAST Num"))
+// var res  = sum(a+b)
+// alert(res)
+
+// Task 4,5,6
+// CALCULATOR
+// function calc(num1,operator,num2){
+// if(operator === "+"){
+// return num1 + num2
+// }
+// else if(operator === "-"){
+// return num1 - num2
+// }
+// else if(operator === "*"){
+// return num1 * num2
+// }
+// else if(operator === "/"){
+// return num1 / num2
+// }
+// }
+// var num1 = Number(prompt("enter first num"))
+// var operator = prompt("enter your operator")
+// var num2 = Number(prompt("enter second num "))
+// var res = calc(num1,operator,num2)                                                                                                
+// alert(res)
