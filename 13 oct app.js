374479e1ca537906ed792e1c52fdc8e1c2574572
Ayan776 <143824843+Ayan776@users.noreply.github.com>
2023-10-13 09:21:16 -0700

    Add files via upload

---
+++ 13 oct app.js
@@ -0,0 +1,44 @@
+/*
+var heading = document.createElement("h1")
+var headingtext = document.createTextNode("Hello world")
+heading.appendChild(headingtext)
+var div = document.getElementById("main")
+div.appendChild(heading)
+console.log(heading)
+
+var headingSmall = document.createElement("h2")
+var headingSmalltext = document.createTextNode("CREATING DOM ELEMENTS")
+headingSmall.appendChild(headingSmalltext)
+var divv = document.getElementById("not main")
+div.appendChild(headingSmall)
+console.log(headingSmall)
+
+var para = document.createElement("p")
+var paratext =document.createTextNode("DOM stands for Document Object Model")
+para.appendChild(paratext)
+var divvv = document.getElementById("not mainly main")
+div.appendChild(para)
+console.log(para)
+
+var paragraph = document.createElement("p")
+var paragraphtext = document.createTextNode("We are creating elements with the help of DOM")
+paragraph.appendChild(paragraphtext)
+var divvvv = document.getElementById("not mainly main idk")
+div.appendChild(paragraph)
+console.log(paragraph)*/
+
+// NOT COMPLETETED
+// var link = document.createElement("a")
+// var linktextOutside = document.createTextNode("google")
+// link.appendChild(linktextOutside)
+// link.setAttribute=("href","https://www.google.com")
+// var div = document.getElementById("linking")
+// div.appendChild(link)
+// console.log(link)
+
+// var image = document.createElement("img")
+// image.setAttribute("src","https://wallpapercave.com/wp/JJqFjxm.jpg")
+// image.setAttribute("width","700")
+// var div = document.getElementById("linking")
+// div.appendChild(image)
+// console.log(image)
